e39499bbe4d9f90aa3d3f15c31dc7790b6e8e570
John Lowvale <johnlowvale@hotmail.com>
2017-05-24 20:05:52 +0700

    base code

---
+++ src/main/java/datdq/sms/App.java
@@ -0,0 +1,13 @@
+package datdq.sms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
